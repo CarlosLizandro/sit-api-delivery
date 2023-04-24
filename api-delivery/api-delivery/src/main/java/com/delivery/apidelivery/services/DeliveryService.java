430f45f1fd0710946e75bc2ac1e47276a0c0a59b
CarlosLizandro <creyesp6@upao.edu.pe>
2023-04-23 22:46:18 -0500

    feat: Crear la clase deliveryService. Implemento los metodos getAllPlates y addPlate

--- api-delivery/api-delivery/src/main/java/com/delivery/apidelivery/services/DeliveryService.java
+++ api-delivery/api-delivery/src/main/java/com/delivery/apidelivery/services/DeliveryService.java
@@ -1,4 +1,25 @@
 package com.delivery.apidelivery.services;
 
+import com.delivery.apidelivery.entities.Plate;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class DeliveryService {
+    private List<Plate> plateMenu;
+
+    public DeliveryService(){
+        this.plateMenu = new ArrayList<>();
+    }
+    //TODO: US-01 Como usuario, quiero poder ver un menú de comida para seleccionar lo que
+    //quiero pedir.
+    public List<Plate> getAllPlates(){
+        return this.plateMenu;
+    }
+
+    //TODO: US-02 Como usuario, quiero poder hacer un pedido y agregar varios platos.
+
+    public void addPlate(Plate plate){
+        this.plateMenu.add(plate);
+    }
 }
