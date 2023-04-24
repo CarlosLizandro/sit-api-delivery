e893033bb2950279d13b0b59d48e91d54ed1a514
CarlosLizandro <creyesp6@upao.edu.pe>
2023-04-23 22:34:39 -0500

    feat: Crea la entidad Plate

--- api-delivery/api-delivery/src/main/java/com/delivery/apidelivery/entities/Plate.java
+++ api-delivery/api-delivery/src/main/java/com/delivery/apidelivery/entities/Plate.java
@@ -4,5 +4,35 @@ public class Plate {
 
     private String name;
     private String price;
-    private String Description;
+    private String description;
+
+    public Plate(String name, String price, String description) {
+        this.name = name;
+        this.price = price;
+        this.description = description;
+
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
